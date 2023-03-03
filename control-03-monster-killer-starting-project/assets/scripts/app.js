f73f6310b5ce82c05f66f18c0f5e7b6f4cfdf974
ediaz13 <diazemanuel27@gmail.com>
2023-03-03 19:50:13 -0300

    100. The "for-of" Loop

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -240,10 +240,21 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  for (let i = 0; i < 3; i++) {
+  /* for (let i = 0; i < 3; i++) {
     console.log('-------------');
   }
-  console.log(battleLog);
+
+  for (let i = 0; i < battleLog.length; i++ ) {
+    console.log(battleLog[i])
+  } */
+
+  let i = 0;
+
+  for (const logEntry of battleLog) {
+    console.log(logEntry);
+    console.log(i);
+    i++;
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
