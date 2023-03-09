75c72b82d527f77f92540f54bd29fd61dfe1012b
ediaz13 <diazemanuel27@gmail.com>
2023-03-09 14:22:17 -0300

    156. Traversing Child Nodes

--- dom/app.js
+++ dom/app.js
@@ -1,6 +1,6 @@
 const h1 = document.getElementById('main-title');
 
-h1.textContent = 'Some new title!';
+h1.textContent = 'Some new title bitch!';
 h1.style.color = 'white';
 h1.style.backgroundColor = 'black';
 
