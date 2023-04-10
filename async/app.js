aaca103c57abcc8eb0e491a957a6781e7a9ae834
ediaz13 <diazemanuel27@gmail.com>
2023-04-10 15:47:36 -0300

    353. Multiple Callbacks & setTimeout(0)

--- async/app.js
+++ async/app.js
@@ -3,13 +3,18 @@ const output = document.querySelector("p");
 
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
-    (posData) => {
-      console.log(posData);
+    posData => {
+      setTimeout(() => {
+        console.log(posData);
+      }, 5000);
     },
     (error) => {
       console.log(error);
     }
   );
+  setTimeout(() => {
+    console.log("Timer Done!!!");  
+  }, 0);
   console.log("Getting position...");
 }
 
