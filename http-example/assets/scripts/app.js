cc15ac0e023c6c82f531213fe3f709ef5cc26163
ediaz13 <diazemanuel27@gmail.com>
2023-04-25 17:48:43 -0300

    371. JSON Data Deep Dive

--- http-example/assets/scripts/app.js
+++ http-example/assets/scripts/app.js
@@ -1,5 +1,23 @@
 const xhr = new XMLHttpRequest();
 
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+    //const listOfPosts = JSON.parse(xhr.response);
+    const listOfPosts = xhr.response;
+    for(post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+    }
+
+    console.log(listOfPosts);
+}
 
 xhr.send();
