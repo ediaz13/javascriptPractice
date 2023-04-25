612e1def46ba92fbd2ba61976547cae7556a7243
ediaz13 <diazemanuel27@gmail.com>
2023-04-25 18:02:59 -0300

    372. Promisifying Http Requests (XMLHttpRequest)

--- http-example/assets/scripts/app.js
+++ http-example/assets/scripts/app.js
@@ -1,23 +1,37 @@
-const xhr = new XMLHttpRequest();
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
 
-const listElement = document.querySelector('.posts');
-const postTemplate = document.getElementById('single-post');
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.responseType = "json";
 
-xhr.responseType = 'json';
+    xhr.onload = function () {
+      resolve(xhr.response);
+      //const listOfPosts = JSON.parse(xhr.response);
+    };
 
-xhr.onload = function() {
-    //const listOfPosts = JSON.parse(xhr.response);
-    const listOfPosts = xhr.response;
-    for(post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        listElement.append(postEl);
-    }
+    xhr.send();
+  });
 
-    console.log(listOfPosts);
+  return promise;
 }
 
-xhr.send();
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  const listOfPosts = responseData;
+  for (post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+}
+
+fetchPosts();
