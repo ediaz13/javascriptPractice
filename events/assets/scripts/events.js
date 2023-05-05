32cc41819d98663137133485540d1dabbede3607
ediaz13 <diazemanuel27@gmail.com>
2023-05-05 20:11:05 -0300

    308. The "event" Object

--- events/assets/scripts/events.js
+++ events/assets/scripts/events.js
@@ -1,6 +1,8 @@
-const button = document.querySelector('button');
+//const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
 const buttonClickHandler = event => {
+    event.target.disabled = true;
     console.log(event);
 }
 
@@ -9,8 +11,15 @@ const anotherButtonClickHandler = () => {
 }
 
 
-button.addEventListener('click', buttonClickHandler);
+//button.addEventListener('click', buttonClickHandler);
 
+/*
 setTimeout(() => {
     button.removeEventListener('click', buttonClickHandler)
 }, 2000);
+*/
+
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', buttonClickHandler);
+});
